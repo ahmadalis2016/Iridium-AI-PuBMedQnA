9f959051ef323fb8d6621df0df10d41c8f5f7c10
Ahmad Shaik <130319416+ahmadalis2016@users.noreply.github.com>
2025-02-08 12:18:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from haystack import component, Document
 from haystack import Pipeline
 from haystack.components.generators.chat import HuggingFaceTGIChatGenerator
+from haystack.dataclasses import ChatMessage
 from haystack.components.builders.prompt_builder import PromptBuilder
 from dotenv import load_dotenv
 from pymed import PubMed
